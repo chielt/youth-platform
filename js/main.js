c708afe20407fa8a7a491f37febfa497e1d9f3a6
Michiel Dekker <chielt@gmail.com>
2014-07-05 14:44:11 +0200

    Final version of prototype

--- js/main.js
+++ js/main.js
@@ -1,12 +1,34 @@
+var MyDoc = {};
+
 $( document ).ready(function() {
+  MyDoc.like = true;
+
+  $('textarea').autosize();
+
+  $('.comment-link').click(function(){ $(this).siblings('.comment-pane').show('fast');});
+  $('.like-link').on('click', function(e) {
+    if(MyDoc.like)
+    {
+      var likes = parseInt($(this).siblings('a').children('.like-counter').html())+1;
+      $(this).siblings('a').children('.like-counter').html(likes);
+      MyDoc.like = false;
+    }
+  });
+
   $('#btn-support-campaign').on('click', function(e) {
     var btnsupport = $(this);
     btnsupport.toggleClass("btn-primary btn-success"); //you can list several class names 
 
-    btnsupport.html() == "Support this campaign"
-    ? btnsupport.html('<span class="glyphicon glyphicon-ok"></span> Supporting')
-    : btnsupport.html('Support this campaign');
-    //e.preventDefault();
+    if(btnsupport.html() == "Support this campaign")
+    {
+      btnsupport.html('<span class="glyphicon glyphicon-ok"></span> Supporting');
+      var cnt = parseInt($('#campaign-supporter-counter').html())+1; 
+      $('#campaign-supporter-counter').html(cnt);
+    } else {
+      btnsupport.html('Support this campaign');
+      var cnt = parseInt($('#campaign-supporter-counter').html())-1; 
+      $('#campaign-supporter-counter').html(cnt);
+    }
   });
 
   $('#btn-follow-campaign').on('click', function(e) {
@@ -14,10 +36,79 @@ $( document ).ready(function() {
  
     btnfollow.toggleClass("btn-primary btn-success"); //you can list several class names 
 
-    btnfollow.html() == "Follow this campaign"
-    ? btnfollow.html('<span class="glyphicon glyphicon-ok"></span> Following')
-    : btnfollow.html('Follow this campaign');
-    //e.preventDefault();
+    if(btnfollow.html() == "Follow this campaign")
+    {
+      btnfollow.html('<span class="glyphicon glyphicon-ok"></span> Following');
+      var cnt = parseInt($('#campaign-follower-counter').html())+1; 
+      $('#campaign-follower-counter').html(cnt);
+    } else {
+      btnfollow.html('Follow this campaign');
+      var cnt = parseInt($('#campaign-follower-counter').html())-1; 
+      $('#campaign-follower-counter').html(cnt);
+    }
+  });
+
+  $('#btn-support-event').on('click', function(e) {
+    var btnsupport = $(this);
+    btnsupport.toggleClass("btn-primary btn-success"); //you can list several class names 
+
+    if(btnsupport.html() == "Join this event")
+    {
+      btnsupport.html('<span class="glyphicon glyphicon-ok"></span> Going');
+      var cnt = parseInt($('#event-going-counter').html())+1; 
+      $('#event-going-counter').html(cnt);
+      if($('#btn-maybe-support-event').html() =='<span class="glyphicon glyphicon-ok"></span> Maybe going')
+      {
+        $('#btn-maybe-support-event').toggleClass("btn-primary btn-success");
+        $('#btn-maybe-support-event').html('Maybe join this event');
+        var cnt = parseInt($('#event-maybe-going-counter').html())-1; 
+        $('#event-maybe-going-counter').html(cnt);
+      }
+    } else {
+      $('#btn-support-event').html('Join this event');
+      var cnt = parseInt($('#event-going-counter').html())-1; 
+      $('#event-going-counter').html(cnt);
+    }
+  });
+
+  $('#btn-maybe-support-event').on('click', function(e) {
+    var btnsupport = $(this);
+    btnsupport.toggleClass("btn-primary btn-success"); //you can list several class names 
+
+    if(btnsupport.html() == "Maybe join this event")
+    {
+      btnsupport.html('<span class="glyphicon glyphicon-ok"></span> Maybe going');
+      var cnt = parseInt($('#event-maybe-going-counter').html())+1; 
+      $('#event-maybe-going-counter').html(cnt);
+      if($('#btn-support-event').html() =='<span class="glyphicon glyphicon-ok"></span> Going')
+      {
+        $('#btn-support-event').toggleClass("btn-primary btn-success");
+        $('#btn-support-event').html('Join this event');
+        var cnt = parseInt($('#event-maybe-going-counter').html())-1; 
+        $('#btn-support-event-counter').html(cnt);
+      }
+    } else {
+      btnsupport.html('Maybe join this event');
+      var cnt = parseInt($('#event-maybe-going-counter').html())-1; 
+      $('#event-maybe-going-counter').html(cnt);
+    }
+  });
+
+  $('#btn-follow-event').on('click', function(e) {
+    var btnfollow = $(this);
+ 
+    btnfollow.toggleClass("btn-primary btn-success"); //you can list several class names 
+
+    if (btnfollow.html() == "Follow this event")
+    {
+      btnfollow.html('<span class="glyphicon glyphicon-ok"></span> Following');
+      var cnt = parseInt($('#event-follower-counter').html())+1; 
+      $('#event-follower-counter').html(cnt);
+    } else {
+      btnfollow.html('Follow this event');
+      var cnt = parseInt($('#event-follower-counter').html())-1; 
+      $('#event-follower-counter').html(cnt);
+    }
   });
 
   $('.btn-going').on('click', function(e) { 
@@ -66,6 +157,16 @@ $( document ).ready(function() {
       $(this).unbind('click');
     });
   });
+
+  $(".responsive-calendar").responsiveCalendar({
+          time: '2014-06',
+          events: {
+            "2014-06-15": {"number": 1, "url": "http://w3widgets.com/responsive-slider"},
+            "2013-04-26": {"number": 1, "url": "http://w3widgets.com"}, 
+            "2013-05-03":{"number": 1}, 
+            "2013-06-12": {}}
+  });
+
 });
 
 $('#campaign-status-input').focusin(function(){
